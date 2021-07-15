84097d8076daa639226ad88ba7bdd361b5be839f
Atsushi <shimarisu.araiguma@gmail.com>
2021-07-15 21:26:37 +0900

    feat: Add save button and id in MemoItem

--- src/components/MemoItem.js
+++ src/components/MemoItem.js
@@ -1,8 +1,17 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Input from '@material-ui/core/Input';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { green } from '@material-ui/core/colors';
 
 export const MemoItem = () => {
+  const theme = createTheme({
+    palette: {
+      primary: green,
+    },
+  });
+
   const [value, setValue] = useState('');
 
   const handleChange = (event) => {
@@ -13,21 +22,29 @@ export const MemoItem = () => {
     <div>
       <div style={{ marginBottom: 50 }}>
         <Input
-          id="standard-multiline-flexible"
+          id="memo-title"
+          type="text"
           fullWidth
           placeholder="Titleを入力してください"
           value={value}
           onChange={handleChange}
         />
       </div>
-      <div>
+      <div style={{ marginBottom: 20 }}>
         <TextField
-          id="standard-multiline-static"
+          id="memo-content"
           multiline
           fullWidth
           rows="10"
           placeholder="Contentを入力してください"
         />
+      </div>
+      <div>
+        <ThemeProvider theme={theme}>
+          <Button id="save-memo" variant="contained" color="primary">
+            Save
+          </Button>
+        </ThemeProvider>
       </div>
     </div>
   );
