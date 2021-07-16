a5db399c388fd2527878166478982a1ad2a4c812
Atsushi <shimarisu.araiguma@gmail.com>
2021-07-16 10:13:46 +0900

    fix: Fix timeout

--- src/apis/informations.js
+++ src/apis/informations.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'http://challenge-server.tracks.run/memoapp',
-  timeout: 5 * 60 * 60,
+  timeout: 2 * 60 * 60, // 2時間(7200秒)でタイムアウト
 });
